5658e07c2524d5b980177261ef598bc8a71662d3
Byron Liu <35056093+kinkypotato45@users.noreply.github.com>
2023-04-21 03:17:51 -0600

    removed using AspNetCore

--- LMS/Controllers/StudentController.cs
+++ LMS/Controllers/StudentController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AspNetCore;
 using LMS.Models.LMSModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
